f712d10b18639446ae53eb7b7a52d59c04ee8e2a
Jeremy <jeremypisces04@gmail.com>
2024-01-04 16:58:15 +0700

    Just to save file

--- server/controllers/transactions.js
+++ server/controllers/transactions.js
@@ -1,38 +1,60 @@
+const nodemailer = require("nodemailer");
+const path = require("path");
+const fs = require("fs");
+const ejs = require("ejs");
+
 const Transaction = require("../models/transaction");
+const Room = require("../models/room");
 const User = require("../models/user");
+const Hotel = require("../models/hotel");
 
-exports.addTransaction = (req, res, next) => {
-  User.findById(req.userId)
-    .then((user) => {
-      const username = user.username;
-      const hotel = req.body.hotel;
-      const room = req.body.room;
-      const dateStart = req.body.dateStart + "T00:00:00Z";
-      const dateEnd = req.body.dateEnd + "T00:00:00Z";
-      const price = req.body.price;
-      const payment = req.body.payment;
-      const status = req.body.status;
+const transport = nodemailer.createTransport({
+  host: "smtp.gmail.email",
+  service: "gmail",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "phvinhquang63@gmail.com",
+    pass: "khxg pxhm zdnk jdab",
+  },
+});
 
-      const transaction = new Transaction({
-        user: username,
-        hotel: hotel,
-        room: room,
-        dateStart: dateStart,
-        dateEnd: dateEnd,
-        price: price,
-        payment: payment,
-        status: status,
-      });
+exports.addTransaction = async (req, res, next) => {
+  let customerEmail, customerFullname;
+  try {
+    const user = await User.findById(req.userId);
 
-      return transaction.save();
-    })
-    .then(() => {
-      res.status(201).json("Reservation created !!!");
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(501).json("Can not create reservation");
+    const username = user.username;
+    const hotel = req.body.hotel;
+    const room = req.body.room;
+    const dateStart = req.body.dateStart + "T00:00:00Z";
+    const dateEnd = req.body.dateEnd + "T00:00:00Z";
+    const price = req.body.price;
+    const payment = req.body.payment;
+    const status = req.body.status;
+
+    customerEmail = user.email;
+    customerFullname = user.fullName;
+
+    const transaction = new Transaction({
+      user: username,
+      hotel: hotel,
+      room: room,
+      dateStart: dateStart,
+      dateEnd: dateEnd,
+      price: price,
+      payment: payment,
+      status: status,
     });
+
+    await transaction.save();
+
+    // Gửi response
+    res.status(201).json("Reservation created !!!");
+  } catch (err) {
+    // console.log(err);
+    res.status(501).json("Can not create reservation");
+  }
 };
 
 exports.getTransactions = (req, res, next) => {
@@ -50,3 +72,39 @@ exports.getTransactions = (req, res, next) => {
       res.status(500).json("Something went wrong");
     });
 };
+
+async function sendConfirmationEmail(customerFullname, customerEmail, rooms) {
+  try {
+    // Xử lý dữ liệu
+    const hotelInfo = await Hotel.findById(req.body.hotel);
+    const hotelName = hotelInfo.name;
+
+    const roomIds = rooms.map((r) => r.roomId);
+
+    const emailPath = path.join(
+      __dirname,
+      "../",
+      "views",
+      "email-confirm-order.ejs"
+    );
+
+    const emailToSend = await ejs.renderFile(emailPath, {
+      data: {
+        fullname: customerFullname,
+        email: customerEmail,
+        hotel: hotelName,
+        // address: customerAddress,
+      },
+    });
+
+    // Gửi email
+    await transport.sendMail({
+      from: "Fake Booking.com <booking@gmail.com>", // sender address
+      to: customerEmail,
+      subject: "Fake Booking.com - Reservation Confirm",
+      html: emailToSend,
+    });
+  } catch (err) {
+    throw err;
+  }
+}
